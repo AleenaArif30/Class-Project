e330cbfa1538d52064413e83fc54d09641275724
AleenaArif30 <aleena.arif2024@gmail.com>
2025-04-11 17:10:45 +0500

    class project

--- backend/index.mjs
+++ backend/index.mjs
@@ -14,10 +14,10 @@ const app = express();
 app.use(
 	cors({
 		origin: ['http://localhost:5174',
-			 'http://localhost:5173',
-			'https://mer-nstack-batch11-section-j-nu.vercel.app',
-			
-			],
+			'http://localhost:5173',
+			'https://mern-stack-project-ecommerce-three.vercel.app',
+
+		],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
@@ -27,15 +27,15 @@ app.use(
 
 app.use(express.json());
 const port = 5000;
-app.use("/api/auth",userRoutes)
-app.use("/api/cart",cartRoutes)
+app.use("/api/auth", userRoutes)
+app.use("/api/cart", cartRoutes)
 app.use('/api/products', productRoutes);
 
 app.use("/", (req, res, next) => {
-  console.log("Request URL:", req.url, "method: ", req.method);
-  next();
+	console.log("Request URL:", req.url, "method: ", req.method);
+	next();
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
